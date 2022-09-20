81321aa1514a9f69b43fb39f640dbeea289bac6c
sreerajsree <sreeandsreeraj@gmail.com>
2022-09-20 16:31:22 +0530

    spread

---
+++ spreadoperator.js
@@ -0,0 +1,19 @@
+var obj = {
+    a: 1,
+    b: 2,
+    c: 'Hello'
+}
+
+var obj2 = {
+    ...obj
+}
+
+var c = [1,2,3,4,5]
+
+var d =[...c]
+
+console.log(obj);
+console.log(obj2);
+console.log(c);
+console.log(d);
+
